5f66260823d1ebf18b0ff3ac6bc63159b4a5b349
Anish Walawalkar <aswalawa@uwaterloo.ca>
2016-02-10 23:51:48 -0500

    updated range_weekdays to return [] when end date < start date

--- weekdays/weekdays.py
+++ weekdays/weekdays.py
@@ -5,25 +5,39 @@ __all__ = [
 
 import datetime
 
+
 def is_weekday(date):
+    '''Checks if the date provided as an arguement is a weekday or not'''
     return True if date.weekday() in range(0, 5) else False
 
 def weekdays_passed(start, end):
+    '''
+    returns the numbers of weekdays between the start date and end date
+    note: start > end
+    '''
     return weekdays_until(end, start)
 
 def weekdays_until(start, end):
+    '''
+    returns the numbers of weekdays between the start date and end date
+    note : end > start
+    '''
     if end < start:
         return 0
     daydiff = end.weekday() - start.weekday()
     return ((end-start).days - daydiff) / 7 * 5 + min(daydiff,5) - (max(end.weekday() - 4, 0) % 5)
 
 def range_weekdays(start, end):
+    '''returns a generator that can be used to iterate over the weekdays in the range start to end-1'''
+    if start > end:
+        return []
     while not start == end:
         if is_weekday(start):
             yield start
         start += datetime.timedelta(days=1)
 
 def prev_weekday(date, num_days=0):
+    '''returns "date" if "date" is a weekday else the previous weekday. num_days is used to specify "how many weekdays before current date"'''
     def inner(date):
         if is_weekday(date):
             return date
@@ -37,6 +51,7 @@ def prev_weekday(date, num_days=0):
     return inner(new_date)
 
 def next_weekday(date, num_days=0):
+    '''returns "date" if "date" is a weekday else the next weekday. num_days is used to specify "how many weekdays after current date"'''
     def inner(date):
         if is_weekday(date):
             return date
