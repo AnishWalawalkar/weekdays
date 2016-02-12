85209d1c1f2076bd28d610a18beca88040ebeb7d
Anish Walawalkar <aswalawa@uwaterloo.ca>
2016-02-11 21:53:25 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="weekdays",
+    version="0.1.0",
+    packages=['weekdays'],
+    description='a module to work with weekdays',
+    author='Anish Walawalkar',
+    author_email='anish.sanjiv@gmail.com',
+    url='https://github.com/AnishWalawalkar/weekdays.git',
+    license='https://opensource.org/licenses/MIT',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'License :: The MIT License (MIT)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ),
+    keywords='datetime weekdays'
+)
