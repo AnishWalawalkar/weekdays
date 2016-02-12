8f274227eef057d5de436e63b763b5882cded3c9
Anish Walawalkar <aswalawa@uwaterloo.ca>
2016-02-11 19:50:44 -0500

    changed next_weekday to return the next weekday instead of the same weekday when num_days=0 and the same for prev_weekday

--- weekdays/weekdays.py
+++ weekdays/weekdays.py
@@ -3,7 +3,7 @@ __all__ = [
     'range_weekdays', 'prev_weekdays', 'next_weekday'
 ]
 
-import datetime
+from datetime import timedelta
 
 
 def is_weekday(date):
@@ -27,40 +27,39 @@ def weekdays_until(start, end):
     daydiff = end.weekday() - start.weekday()
     return ((end-start).days - daydiff) / 7 * 5 + min(daydiff,5) - (max(end.weekday() - 4, 0) % 5)
 
-def range_weekdays(start, end):
+def date_range(start, end):
     '''returns a generator that can be used to iterate over the weekdays in the range start to end-1'''
     if start > end:
         return []
     while not start == end:
         if is_weekday(start):
             yield start
-        start += datetime.timedelta(days=1)
+        start += timedelta(days=1)
+
+def weekend_offset(date, flag=False):
+    if date.weekday() == 5:
+        return 2 if flag else 1
+    return 1 if flag else 2
 
 def prev_weekday(date, num_days=0):
-    '''returns "date" if "date" is a weekday else the previous weekday. num_days is used to specify "how many weekdays before current date"'''
-    def inner(date):
-        if is_weekday(date):
-            return date
-
-        day_count = 1 if date.weekday() == 5 else 2
-        return date - datetime.timedelta(days=day_count)
-
+    '''return the previous weekday'''
+    if num_days < 0:
+        return date
     if not num_days:
-        return inner(date)
-    new_date = date - datetime.timedelta(days=num_days)
-    return inner(new_date)
+        num_days = 1
+
+    date -= timedelta(days=num_days)
+
+    return date if is_weekday(date) else date - timedelta(days=weekend_offset(date))
 
 def next_weekday(date, num_days=0):
-    '''returns "date" if "date" is a weekday else the next weekday. num_days is used to specify "how many weekdays after current date"'''
-    def inner(date):
-        if is_weekday(date):
-            return date
-
-        day_count = 2 if date.weekday() == 5 else 1
-        return date + datetime.timedelta(days=day_count)
-
+    '''return the next weekday'''
+    if num_days < 0:
+        return date
     if not num_days:
-        return inner(date)
+        num_days = 1
 
-    new_date = date + datetime.timedelta(days=num_days)
-    return inner(new_date)
+    date += timedelta(days=num_days)
+
+    return date if is_weekday(date) else date + timedelta(days=weekend_offset(date, flag=True))
+
