64cb1f71cfb8cc5e3a17366421cf45915807dbaf
Anish Walawalkar <aswalawa@uwaterloo.ca>
2016-02-11 23:37:38 -0500

    verion bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyweekdays",
-    version="0.1.0",
+    version="0.1.1",
     packages=['weekdays'],
     description='a module to work with weekdays',
     author='Anish Walawalkar',
