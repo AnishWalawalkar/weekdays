8fbb072b5247235a35dc6fc7a5637251c64fa180
anish walawalkar <anish@jewlr.com>
2016-02-24 11:52:00 -0500

    improved code quality

--- weekdays/weekdays.py
+++ weekdays/weekdays.py
@@ -1,15 +1,18 @@
+'''python module to work with weekdays'''
+import time
+from datetime import timedelta
+from datetime import datetime
+
 __all__ = [
     'is_weekday', 'weekdays_passed', 'weekdays_until',
     'date_range', 'prev_weekday', 'next_weekday'
 ]
 
-import time
-from datetime import timedelta
-from datetime import datetime
-
 def time_to_datetime(time_struct=None, time_in_seconds=None):
+    '''heloper function to convert time object to datetime object'''
     if not  (time_struct or time_in_seconds):
-        raise Exception('One of "time_struct" or "time_in_seconds" keyword arguements must be provided')
+        raise Exception(
+            'One of "time_struct" or "time_in_seconds" keyword arguements must be provided')
     if time_struct:
         time_in_seconds = time.mktime(time_struct)
     return datetime.fromtimestamp(time_in_seconds)
@@ -33,7 +36,7 @@ def weekdays_until(start, end):
     if end < start:
         return 0
     daydiff = end.weekday() - start.weekday()
-    return ((end-start).days - daydiff) / 7 * 5 + min(daydiff,5) - (max(end.weekday() - 4, 0) % 5)
+    return ((end-start).days - daydiff) / 7 * 5 + min(daydiff, 5) - (max(end.weekday() - 4, 0) % 5)
 
 def date_range(start, end):
     '''returns a generator that can be used to iterate over the weekdays in the range start to end-1'''
@@ -45,6 +48,7 @@ def date_range(start, end):
         start += timedelta(days=1)
 
 def weekend_offset(date, flag=False):
+    '''internal helper function'''
     if date.weekday() == 5:
         return 2 if flag else 1
     return 1 if flag else 2
